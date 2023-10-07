b563b9f661f43753bceb76ba88236f9972ccc538
victoryw <victoryw@victoryws-MacBook-Pro.local>
2023-10-07 22:18:15 +0800

    feat #2 update the deploy to us-east-2 region

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-aws cloudformation create-stack \
+aws cloudformation --region us-east-2 create-stack \
   --stack-name stable-diffusion-ui-ec2 \
   --template-body file://./stable-diffusion.yaml
+
+
